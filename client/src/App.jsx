504fe94e393262154694ae8203c4536690e01130
Om-1004 <opatel101004@gmail.com>
2025-08-15 18:22:15 -0400

    firebase in progress

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,9 @@
+import SignUpPage from "./pages/SignUpPage";
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-      
-    </h1>
+    <div>
+      <SignUpPage />
+    </div>
   )
 }
