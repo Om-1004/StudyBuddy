1963cfa68d745fc0223d4363e1479a52c49f5deb
Shivam Patel <s.pate0407@gmail.com>
2025-08-16 13:42:53 -0400

    fixed some bugs

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,24 +1,26 @@
 import SignUpPage from "./pages/SignUpPage.jsx";
-import HomePage from "./components/HomePage.jsx"
+import HomePage from "./pages/HomePage.jsx"
 import Listings from "./pages/Listings.jsx"
 import SignInPage from "./pages/LoginPage.jsx"
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LoginPage from "./pages/LoginPage.jsx";
 import BrowsePage from "./pages/BrowsePage.jsx";
+import Navbar from "./components/Navbar.jsx";
 
 export default function App() {
   return <BrowserRouter>
-  <Routes>
-    <Route path='/homepage' element={<HomePage /> } />
-    <Route path='/signup' element= {<SignUpPage />} />
-    <Route path='/login' element= {<LoginPage />} />
-    <Route path='/browse' element= {<BrowsePage />} />
+    <Navbar />
+    <Routes>
+      <Route path='/homepage' element={<HomePage />} />
+      <Route path='/signup' element={<SignUpPage />} />
+      <Route path='/login' element={<LoginPage />} />
+      <Route path='/browse' element={<BrowsePage />} />
 
-    <Route path='/listings' element={<Listings />} />
-    <Route path='/signin' element={<SignInPage />} />
+      <Route path='/listings' element={<Listings />} />
+      <Route path='/signin' element={<SignInPage />} />
 
-  </Routes>
-  
+    </Routes>
+
   </BrowserRouter>
 
 }
