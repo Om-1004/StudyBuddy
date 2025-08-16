004e545e22fd8345806657c0454f7a6c685e276b
jimit.rawal <jimitrawal15@gmail.com>
2025-08-16 13:01:34 -0400

    Login-signup fixed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,8 @@
 import SignUpPage from "./pages/SignUpPage.jsx";
 import HomePage from "./components/HomePage.jsx"
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LoginPage from "./pages/LoginPage.jsx";
+import BrowsePage from "./pages/BrowsePage.jsx";
 
 
 export default function App() {
@@ -8,6 +10,8 @@ export default function App() {
   <Routes>
     <Route path='/homepage' element={<HomePage /> } />
     <Route path='/signup' element= {<SignUpPage />} />
+    <Route path='/login' element= {<LoginPage />} />
+    <Route path='/browse' element= {<BrowsePage />} />
   </Routes>
   
   </BrowserRouter>
